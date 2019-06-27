a2ea08336fc0b0bcdb3bc49d4c4c268a5527278a
Sean <me@ininger.me>
2019-06-26 20:03:37 -0700

    add some colorize

--- lib/game_board.rb
+++ lib/game_board.rb
@@ -1,4 +1,5 @@
 # 7 Across, 6 deep is connect four
+require 'colorize'
 
 class GameBoard
 
@@ -25,7 +26,7 @@ class GameBoard
     board
   end
 
-  def place_checker(column, game_board = board, player = 'X')
+  def place_checker(column, game_board = board, player = '( )')
     (board.length - 1).downto(0) do |iter|
       if game_board[iter][column].empty?
         game_board[iter][column] = player
@@ -37,4 +38,11 @@ class GameBoard
   def whose_turn(player = player_turn)
     player == 'red' ? 'black' : 'red'
   end
+
+  def player_piece(color = player_turn)
+    '( )'.colorize(color.to_sym)
+  end
 end
+
+game = GameBoard.new
+puts game.player_piece
--- spec/game_board_spec.rb
+++ spec/game_board_spec.rb
@@ -20,9 +20,9 @@ RSpec.describe 'GameBoard' do
       game_board = GameBoard.new
       board_grid = game_board.build_board
       game_board.place_checker(0, board_grid)
-      expect(board_grid[-1]).to eql(['X', '', '', '', '', '', ''])
+      expect(board_grid[-1]).to eql(['( )', '', '', '', '', '', ''])
       game_board.place_checker(0, board_grid)
-      expect(board_grid[-2]).to eql(['X', '', '', '', '', '', ''])
+      expect(board_grid[-2]).to eql(['( )', '', '', '', '', '', ''])
     end
   end
 
@@ -31,6 +31,13 @@ RSpec.describe 'GameBoard' do
       game_board = GameBoard.new
       expect(game_board.whose_turn('red')).to eql('black')
       expect(game_board.whose_turn('black')).to eql('red')
+    end
+  end
+
+  describe '#player_piece' do
+    it 'returns a red piece for player red, and a black piece otherwise' do
+      game_board = GameBoard.new
+      expect(game_board.player_piece('red').to eql('( )'))
     end
   end
 end
